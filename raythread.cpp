257a7e8c780e10a70044d3f7f53493a8627f3dd1
livalue <dimonawesome@gmail.com>
2021-03-01 11:47:30 +0300

    release v1.0.0

--- raythread.cpp
+++ raythread.cpp
@@ -21,20 +21,6 @@ void RayThread::run()
 
     for (int x = bound.xs; x <= bound.xe; x++){
         for (int y = bound.ys; y <= bound.ye; y++){
-//            float px = (2 * ((float)x / (float)width) - 1) * tan(cam->fov / 2 * M_PI / 180) * cam->aspect_ratio;
-//            float py = (1 - 2 * ((float)y / (float)height)) * tan(cam->fov / 2 * M_PI / 180);
-//            auto d = Vec3f{px, py, 1}.normalize();
-//            qDebug() << "direction 1 = " << d.x << d.y << d.z;
-//            auto d = canvasToViewPort(x, y);
-//            qDebug() << height / 2 << d.x << d.y << d.z;
-//            return;
-//            auto d = toWorld(x, y).normalize();
-//            qDebug() << "direction 2 = " << m.x << m.y << m.z;
-//            auto viewPlanePoint = viewPlaneBottomLeftPoint + xIncVector * (float)x + yIncVector * (float)y;
-//            auto castRay = (viewPlanePoint - cam->position).normalize();
-//            qDebug() << "new_direction = " << castRay.x << castRay.y << castRay.z;
-//            autod = camera_d
-//            auto color = traceRay(origin, d, 1, std::numeric_limits<float>::max(), 1) * 255.f;
             auto d = toWorld(u, v, w_, x, y).normalize();
             auto color = cast_ray(Ray(cam->position, d)) * 255.f;
             img.setPixelColor(x, y, QColor(color.x, color.y, color.z));
